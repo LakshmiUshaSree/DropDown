034207edf6d85858fa608ed9d08c12ae7d69a928
LakshmiUshaSree <lakshmiushasreer@gmail.com>
2022-05-26 17:02:19 +0530

    change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Item } from './item';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'drop-down';
-}
+
+    
+  items:Item[];
+  itemtype: Number;
+  itemSelected:Number;
+
+
+  ngOnInit() {
+
+    this.items=[
+      {id:1,itemtype:"Staff"},
+      {id:2,itemtype:"Admin"},
+      {id:3, itemtype:"Student"}
+    ];
+   this.itemSelected=3;
+    }
+  }
+    
+  
+  
+
+
+
